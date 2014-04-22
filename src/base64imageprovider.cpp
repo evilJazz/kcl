fd4fbbc945a9e35cdc8c19f11d2cb41cba30a093
Wincent Balin <wincent@balin.at>
2014-04-22 17:01:06 +0000

    Implemented image size support for base64 image provider, with scaling.
    git-svn-id: https://svn.katastrophos.net/kcl/trunk@96 799798e9-081a-41c7-a665-637564affb03

--- src/base64imageprovider.cpp
+++ src/base64imageprovider.cpp
@@ -1,5 +1,8 @@
 #include "KCL/base64imageprovider.h"
 
+#include <QBuffer>
+#include <QImageReader>
+
 #ifdef KCL_QTQUICK2
     #define QDeclarativeImageProvider QQuickImageProvider
 #endif
@@ -12,7 +15,39 @@ Base64ImageProvider::Base64ImageProvider() :
 
 QImage Base64ImageProvider::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
 {
-    Q_UNUSED(size)
-    Q_UNUSED(requestedSize)
-    return QImage::fromData(QByteArray::fromBase64(id.toLatin1()));
+    QByteArray imageData = QByteArray::fromBase64(id.toLatin1());
+    QBuffer *imageDevice = new QBuffer(&imageData);
+    imageDevice->open(QIODevice::ReadOnly);
+
+    QImageReader reader;
+    reader.setDecideFormatFromContent(true); // Important!
+    reader.setDevice(imageDevice);
+    QImage originalImage = reader.read();
+    QSize originalSize = originalImage.size();
+    imageDevice->close();
+
+    QImage resultImage;
+    if (originalSize == requestedSize)
+    {
+        if (size)
+            *size = originalSize;
+
+        resultImage = originalImage;
+    }
+    else
+    {
+        QSize fittedSize(originalSize);
+        fittedSize.scale(requestedSize, Qt::KeepAspectRatio);
+        *size = fittedSize;
+
+        imageDevice->open(QIODevice::ReadOnly);
+        reader.setDevice(imageDevice);
+        reader.setScaledSize(fittedSize);
+        resultImage = reader.read();
+        imageDevice->close();
+    }
+
+    delete imageDevice;
+
+    return resultImage;
 }
