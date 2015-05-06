ade08c1f47f5472e0b44041b0ae50087fdcbcaaa
Andre H. Beckedorf <evilJazz@katastrophos.net>
2015-05-06 14:13:51 +0000

    Improved error reporting in MessageOverlay.js.
    git-svn-id: https://svn.katastrophos.net/kcl/trunk@190 799798e9-081a-41c7-a665-637564affb03

--- src/qml/KCL/MessageOverlay.js
+++ src/qml/KCL/MessageOverlay.js
@@ -1,27 +1,39 @@
 
 function open(parent, delegate, params, autoOpen, looseItem, immediate)
 {
-    var component = Qt.createComponent(Qt.resolvedUrl("qrc:/KCL/MessageOverlay.qml"));
-    console.log(component.errorString());
+    try
+    {
+        var component = Qt.createComponent(Qt.resolvedUrl("qrc:/KCL/MessageOverlay.qml"));
+        if (component.status === Component.Error)
+        {
+            Debug.print(component.errorString());
+            return null;
+        }
 
-    var p;
-    if (Qt.isQtObject(delegate))
-        p = {"sourceComponent": delegate, "params": params};
-    else
-        p = {"source": delegate};
+        var p;
+        if (Qt.isQtObject(delegate))
+            p = {"sourceComponent": delegate, "params": params};
+        else
+            p = {"source": delegate};
 
-    if (typeof(params) != "undefined")
-        p.params = params;
+        if (typeof(params) != "undefined")
+            p.params = params;
 
-    if (typeof(autoOpen) != "undefined")
-        p.autoOpen = autoOpen;
+        if (typeof(autoOpen) != "undefined")
+            p.autoOpen = autoOpen;
 
-    if (typeof(looseItem) != "undefined")
-        p.looseItem = looseItem;
+        if (typeof(looseItem) != "undefined")
+            p.looseItem = looseItem;
 
-    if (typeof(immediate) != "undefined")
-        p.fadeEnabled = !immediate;
+        if (typeof(immediate) != "undefined")
+            p.fadeEnabled = !immediate;
 
-    var overlay = component.createObject(parent, p);
-    return overlay;
+        var overlay = component.createObject(parent, p);
+        return overlay;
+    }
+    catch (e)
+    {
+        Debug.print(JSON.stringify(e));
+        return null;
+    }
 }
