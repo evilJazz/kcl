46ac68f7234155c12faa32625f92ba43584c9441
Andre H. Beckedorf <evilJazz@katastrophos.net>
2016-05-23 10:02:01 +0000

    Fixed compilation issue on Qt 4 caused by missing QUrl::isLocalFile() method.
    git-svn-id: https://svn.katastrophos.net/kcl/trunk@257 799798e9-081a-41c7-a665-637564affb03

--- src/filesystemutils.cpp
+++ src/filesystemutils.cpp
@@ -121,7 +121,7 @@ QString FileSystemUtils::localPathFromUrl(const QUrl &url)
     if (url.toString().startsWith("qrc:"))
         return url.toString().replace("qrc:", ":");
     else
-        return url.isLocalFile() ? url.toLocalFile() : "";
+        return url.scheme() == "file" ? url.toLocalFile() : "";
 }
 
 QString FileSystemUtils::pathAppend(const QString &path1, const QString &path2)
