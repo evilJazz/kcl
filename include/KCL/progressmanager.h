e2a854a4fb311b88b149465c9f2a1d830680f70e
Andre H. Beckedorf <evilJazz@katastrophos.net>
2016-09-25 16:56:19 +0000

    Updated license boilerplate to reflect triple licensing under LGPL 2.1, LGPL 3.0 and MPL 1.1.
    git-svn-id: https://svn.katastrophos.net/kcl/trunk@265 799798e9-081a-41c7-a665-637564affb03

--- include/KCL/progressmanager.h
+++ include/KCL/progressmanager.h
@@ -1,25 +1,36 @@
-/***************************************************************************
- *   Copyright (C) 2011-2013 Andre Beckedorf                               *
- * 			     <evilJazz _AT_ katastrophos _DOT_ net>                    *
- *                                                                         *
- *   This library is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Lesser General Public License version   *
- *   2.1 as published by the Free Software Foundation.                     *
- *                                                                         *
- *   This library is distributed in the hope that it will be useful, but   *
- *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
- *   Lesser General Public License for more details.                       *
- *                                                                         *
- *   You should have received a copy of the GNU Lesser General Public      *
- *   License along with this library; if not, write to the Free Software   *
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA         *
- *   02110-1301  USA                                                       *
- *                                                                         *
- *   Alternatively, this file is available under the Mozilla Public        *
- *   License Version 1.1.  You may obtain a copy of the License at         *
- *   http://www.mozilla.org/MPL/                                           *
- ***************************************************************************/
+/****************************************************************************
+**
+** Copyright (C) 2011-2016 Andre Beckedorf
+** Contact: <evilJazz _AT_ katastrophos _DOT_ net>
+**
+** This file is part of the Katastrophos.net Component Library (KCL)
+**
+** $KCL_BEGIN_LICENSE$
+** GNU Lesser General Public License Usage
+** This library is free software; you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License version
+** 2.1 or 3.0 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful, but
+** WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Lesser General Public License for more details:
+** https://www.gnu.org/licenses/lgpl-2.1.html
+** https://www.gnu.org/licenses/lgpl-3.0.html
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with this library; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+** 02110-1301  USA
+**
+** Mozilla Public License Usage
+** Alternatively, this file is available under the Mozilla Public
+** License Version 1.1.  You may obtain a copy of the License at
+** http://www.mozilla.org/MPL/
+**
+** $KCL_END_LICENSE$
+**
+****************************************************************************/
 
 #ifndef PROGRESSMANAGER_H
 #define PROGRESSMANAGER_H
